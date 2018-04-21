3f213e30ee825e49f4c323ca718fa8679dd3afd5
rluks <rluks@seznam.cz>
2018-04-21 20:01:18 +0200

    unused var

--- Assets/Scripts/TileCollisionHandler.cs
+++ Assets/Scripts/TileCollisionHandler.cs
@@ -6,18 +6,5 @@ using System;
 
 public class TileCollisionHandler : MonoBehaviour {
 
-    Tilemap tilemap;
-    Vector3Int pPos;
-
     public string HouseSpotTileName;
-
-    // Use this for initialization
-    void Start () {
-        tilemap = GetComponent<Tilemap>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
