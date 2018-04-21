662e5ec8b738a546d3bba3836dd1557db02f989a
Stanislav Láznička <slaz@seznam.cz>
2018-04-21 14:55:08 +0200

    Spawn enemies
    Create an EnemySpawner object which allows for deployment of
    multiple spawnpoints of an enemy with certain type (currently a
    Pope again).

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+    public GameObject enemyType;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+
+
+	// Use this for initialization
+	void Start () {
+        // can be changed to use random intervals by calling Invoke() in Invoke()
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+	}
+	
+
+    void Spawn()
+    {
+        print("hoj sulin");
+        // FIXME: if player is dead, do not spawn
+        if (spawnPoints.Length > 0) {
+            int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+            Instantiate(enemyType, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        }
+    }
+}
