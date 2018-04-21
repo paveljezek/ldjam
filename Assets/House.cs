ce19f0fde226b652df04c7d5c7e057930ca97015
rluks <rluks@seznam.cz>
2018-04-21 18:35:54 +0200

    spawn houses directly at HouseSpotTileName tiles

---
+++ Assets/House.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class House : MonoBehaviour {
+
+    bool isHouseBuild;
+
+    public void BuildHouse()
+    {
+        isHouseBuild = true;
+        sr.enabled = true;
+    }
+
+    public void DestroyHouse()
+    {
+        isHouseBuild = false;
+        sr.enabled = false;
+    }
+
+
+    SpriteRenderer sr;
+
+	// Use this for initialization
+	void Start () {
+        sr = GetComponentInChildren<SpriteRenderer>();
+        //DestroyHouse();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/SpawnHouses.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class SpawnHouses : MonoBehaviour
+{
+    public GameObject HousePrefab;
+
+    void Start()
+    {
+        GridLayout gridLayout = transform.parent.GetComponentInParent<GridLayout>();
+        TileCollisionHandler tch = GetComponent<TileCollisionHandler>();
+
+        Tilemap tilemap = GetComponent<Tilemap>();
+
+        BoundsInt bounds = tilemap.cellBounds;
+        TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
+
+        for (int x = 0; x < bounds.size.x; x++)
+        {
+            for (int y = 0; y < bounds.size.y; y++)
+            {
+                TileBase tile = allTiles[x + y * bounds.size.x];
+                if (tile != null)
+                {              
+                    if (tile.name == tch.HouseSpotTileName)
+                    {
+                        Debug.Log("x:" + x + " y:" + y + " tile:" + tile.name);
+                        print("here is house tile");
+                        GameObject house = Instantiate(HousePrefab, null);
+                        house.name = "muj dum";
+                        Vector3 housePos = gridLayout.CellToWorld(new Vector3Int(x - 13, y - 19, 0));
+                        house.transform.position = housePos;
+                    }
+                }
+            }
+        }
+    }
+}
