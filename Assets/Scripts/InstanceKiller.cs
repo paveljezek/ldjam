e5b61df091b1240b648978a2410b94becd40f016
Stanislav Láznička <slaz@seznam.cz>
2018-04-21 17:23:09 +0200

    Be a lamb and kill everything that falls with fire

---
+++ Assets/Scripts/InstanceKiller.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstanceKiller : MonoBehaviour {
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        print("Killing " + other.name + "with freaking fire");
+        // kill them with fire
+        Destroy(other.gameObject);
+    }
+}
