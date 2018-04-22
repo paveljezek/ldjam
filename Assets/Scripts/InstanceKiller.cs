3596992460559d67183c6047c17e5736628f2be3
rluks <rluks@seznam.cz>
2018-04-22 11:53:08 +0200

    changed player to prefab, fixed colliision with player

--- Assets/Scripts/InstanceKiller.cs
+++ Assets/Scripts/InstanceKiller.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class InstanceKiller : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.name == "Player") {
-            other.gameObject.GetComponent<HealthController>().healthSub(100);
+        print("trigger enter");
+        if(other.gameObject.tag == "Player") {
+            other.gameObject.GetComponentInParent<HealthController>().healthSub(100);
         } else {
             // kill them with fire
             Destroy(other.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        print("on col enter");
+        if (other.gameObject.name == "Player")
+        {
+            other.gameObject.GetComponent<HealthController>().healthSub(100);
         }
     }
 }
