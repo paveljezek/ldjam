24a71b2753f43d063a2e35ea68dbf47e03dd4a6b
paveljezek <paveljezek@outlook.com>
2018-04-22 17:36:56 +0200

    melee attack

---
+++ Assets/Scripts/MeleeHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeHandler : MonoBehaviour {
+
+	private bool canHit = false;
+	private Collider2D who;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		canHit = true;
+		who = other;
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		canHit = false;
+	}
+
+	public bool hit() {
+		return canHit;
+	}
+
+	public Collider2D enemy() {
+		return who;
+	}
+}
